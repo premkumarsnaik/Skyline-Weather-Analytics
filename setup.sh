5c2e9b1046c6b1c4e20a194fa834153d71ca9866
premkumarsnaik <50477936+premkumarsnaik@users.noreply.github.com>
2026-05-06 13:44:08 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# 1. Clone the repository (Replace with your actual GitHub URL)
+!git clone https://github.com/premkumarsnaik/Skyline-Weather-Analytics.git
+
+# 2. Move into the project directory
+%cd Skyline-Weather-Analytics
+
+# 3. Install necessary libraries
+!pip install requests ipywidgets pandas -q
+
+print("✅ Setup Complete. You are ready to run the app!")
